609ca2eee123c00d9bd67835182061782e4f29b3
Reuel Nixon <reuelnixon@gmail.com>
2023-08-23 11:58:49 +0530

    Connected PSQL with the web server

---
+++ database/postgres.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func ConnectToDB() {
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading env file \n", err)
+    }
+
+    dsn := fmt.Sprintf("host=localhost user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Kolkata",
+        os.Getenv("DB_USER"), 
+		os.Getenv("DB_PASSWORD"), 
+		os.Getenv("DB_NAME"), 
+		os.Getenv("DB_PORT"))
+
+    log.Print("Connecting to the Database...")
+    DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+        Logger: logger.Default.LogMode(logger.Info),
+    })
+
+    if err != nil {
+        log.Fatal("Erroe connecting to database. \n", err)
+        os.Exit(2)
+    }
+    log.Println("DB Connection established successfully")
+}
